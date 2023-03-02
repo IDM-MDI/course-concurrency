3c194e63a59e1c421aedd9a855814d8367f24ff5
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-03-02 12:57:24 +0300

    add integration test and delete modifiers

--- src/test/java/ru/clevertec/concurrency/ServerTest.java
+++ src/test/java/ru/clevertec/concurrency/ServerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServerTest {
-    private Server server;
+    Server server;
 
     @BeforeEach
     void setup() {
